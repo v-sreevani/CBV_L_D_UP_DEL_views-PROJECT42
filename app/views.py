ba06cc882d16f4457ad52e10b095407e8bcec394
v-sreevani <vaddisreevani1234@gmail.com>
2023-05-19 17:47:29 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from app.models import *
+# Create your views here.
+from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+class Home(TemplateView):
+    template_name='app/home.html'
+
+class SchoolList(ListView):
+    model=School
+    context_object_name='schools'
