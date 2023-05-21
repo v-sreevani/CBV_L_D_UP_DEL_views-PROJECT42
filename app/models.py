71b65606ea8b51a46a02d59387b57eb848bd383f
v-sreevani <vaddisreevani1234@gmail.com>
2023-05-21 16:38:32 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class School(models.Model):
     name=models.CharField(max_length=100)
@@ -8,6 +8,9 @@ class School(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail',kwargs={'pk':self.pk})
 
 class Student(models.Model):
     school=models.ForeignKey(School,on_delete=models.CASCADE,related_name='students')
--- app/views.py
+++ app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from app.models import *
 # Create your views here.
 from django.views.generic import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
+from django.views.generic import CreateView
 
 class Home(TemplateView):
     template_name='app/home.html'
@@ -10,3 +11,12 @@ class Home(TemplateView):
 class SchoolList(ListView):
     model=School
     context_object_name='schools'
+    template_name='app/school_list.html'
+
+class SchoolDetail(DetailView):
+    model=School
+    context_object_name='sclobject'
+
+class SchoolCreate(CreateView):
+    model=School
+    fields='__all__'
